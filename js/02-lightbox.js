09ffde22e8b6eea90d266d8340f27df99e0fe5c8
zTjamm <jamm85@bk.ru>
2021-10-18 17:15:12 +0300

    11
    22

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.map((img) => `<a class="gallery__item" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"      
+      alt="${img.description}"
+    />
+  </a>`).join("");
+gallery.insertAdjacentHTML("afterbegin", markup)
 
-console.log(galleryItems);
+gallery.addEventListener("click", openModal);
+function openModal(event) {    
+    event.preventDefault();
+    if (event.target.nodeName === "IMG") {
+        let gallerys = new SimpleLightbox('.gallery a',
+            {
+                captionType: Attr,
+                captionsData: event.target.alt,
+                captionDelay: 250,
+            });
+        gallerys.on('show.simplelightbox');
+        console.log(event.target.alt)
+    } else {
+        console.log("lol")
+    }     
+}
+  
